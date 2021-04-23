bff646f5110c63d5dcc3a8340280db0e1529db0a
近藤米功 <foad5969@gmail.com>
2021-04-23 16:41:23 +0900

    five commit

---
+++ testtest/MyOkashi/MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiApp.swift
+//  MyOkashi
+//
+//  Created by admin on 2021/04/18.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
