f64e336d99e9db3639215b5987adec74ac8f90ab
近藤米功 <foad5969@gmail.com>
2021-04-24 10:47:49 +0900

    seven commit

--- dragonball/dragonball3/ContentView.swift
+++ dragonball/dragonball3/ContentView.swift
@@ -104,8 +104,7 @@ struct ContentView: View {
             Button(action: {
                 bottan=bottan+1
                 print("つぎへ")
-                print("つぎへ")
-                print("つぎへ")
+                
             }) {
                 if answerNumber==0{
                     Text("サウンドがヒントだぞ")
